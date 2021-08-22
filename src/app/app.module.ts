5c03454eb106df06d69beaf80ad901ae97e0593a
lanarokip <lankipkoecharo@gmail.com>
2021-08-22 17:25:44 +0300

    add the app.module files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { QuotesComponent } from './components/quotes/quotes.component';
+import { QuoteDetailsComponent } from './components/quote-details/quote-details.component';
+import { QuoteFormComponent } from './components/quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailsComponent,
+    QuoteFormComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]

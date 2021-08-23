c08137977c8920128843a642fb644190c77e7dcb
lanarokip <lankipkoecharo@gmail.com>
2021-08-23 11:06:10 +0300

    added date pipe to calculate the datesubmited

--- src/app/count.pipe.ts
+++ src/app/count.pipe.ts
@@ -6,17 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CountPipe implements PipeTransform {
 
   transform(value: any): number {
-    let today:Date = new Date(); //get current date and time
+    let today:Date = new Date();
     let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(value - todayWithNoTime) //returns value in miliseconds
-    const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
-    var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
+    var dateDifference = Math.abs(todayWithNoTime-value)
+    const secondsInDay = 86400;
+    var dateDifferenceSeconds = dateDifference*0.001;
     var dateCounter = dateDifferenceSeconds/secondsInDay;
-
-    if (dateCounter >= 1 && value > todayWithNoTime){
+    if (dateCounter >= 1) {
       return dateCounter;
-    }else{
-      return 0;
-    }
+    } else
+     return 0;
   }
 }
